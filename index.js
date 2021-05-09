c068896993d715397fe096cde30f46eb6b1b155b
atrusty <trusty.aaron.m@gmail.com>
2021-05-09 12:36:47 -0700

    match gt from incoming query

--- index.js
+++ index.js
@@ -15,24 +15,28 @@ app.listen(port, () => {
     console.log(`CT STATS app listening at http://localhost:${port}`)
 });
 
-//stats init testing
+//individual stats general
 app.get('/myStats', async (req, res) => {
-    
-    let result = await lib.halo.mcc['@0.0.11'].stats({
-        gamertag: "TR Trusty"
+    let gamertag = req.query.gt;
+    let numGames = req.query.numGames;
+    if(numGames == undefined) {
+        numGames = 20;
+    }
+
+    let result = await lib.halo.mcc['@0.0.11'].games.history({
+        gamertag: gamertag,
+        count: numGames
     });
 
-    console.log(result);
-
     retVal = {
-        hi: "hi :)"
+        data: result
     };
 
     res.send(retVal);
 });
 
 
-//stats squad
+//stats squad group
 app.get('/squadStats', async (req, res) => {
 
     let result = await lib.halo.mcc['@0.0.11'].squad.activity({
@@ -89,6 +93,78 @@ app.get('/squadStats', async (req, res) => {
             avgKills+=(stat.kills/numStats);
             avgDeaths+=(stat.deaths/numStats);
             avgAssists+=(stat.assists/numStats);
+        }
+
+        let aggStat = {
+            win: win,
+            avgHeadshotRate: avgHeadshotRate,
+            avgKD: avgKD,
+            avgScore: avgScore,
+            avgKills: avgKills,
+            avgDeaths: avgDeaths,
+            avgAssists: avgAssists,
+            playedAt: playedAt
+        };
+        
+        aggregSqdStats.push(aggStat);
+        
+    }
+
+
+    retVal = {
+        data: aggregSqdStats
+    };
+
+    res.send(retVal);
+
+});
+
+//stats squad individual
+app.get('/squadStatsIndiv', async (req, res) => {
+
+    let gt = req.query.gt;
+
+    let result = await lib.halo.mcc['@0.0.11'].squad.activity({
+        gamertags: [
+            'sagwasswagga',
+            'Cragle 3',
+            'A Pregnant Nunn',
+            'babaganoosh069',
+            'Girthy Jello',
+            'SirPuffingtin',
+            'ya boi nar',
+            'l swordzzz'
+        ],
+        minSquadSize: '3',
+        game: 'Halo 3'
+    });
+
+
+    let games = result.games;
+
+    let aggregSqdStats = [];
+    
+    for(let match of games) {
+        let win = match.squadWon;
+        let avgHeadshotRate = 0.0;
+        let avgKD = 0.0;
+        let avgScore = 0.0;
+        let avgKills = 0.0;
+        let avgDeaths = 0.0;
+        let avgAssists = 0.0;
+        let playedAt = match.playedAt;
+
+        for(let stat of match.stats) {
+            if(stat.gamertag.toUpperCase() == gt.toUpperCase()) {
+                console.log(stat)
+                //aggregate them all here
+                avgHeadshotRate+=(stat.headshotRate);
+                avgKD+=(stat.killDeathRatio);
+                avgScore+=(stat.score);
+                avgKills+=(stat.kills);
+                avgDeaths+=(stat.deaths);
+                avgAssists+=(stat.assists);
+            }
         }
 
         let aggStat = {
