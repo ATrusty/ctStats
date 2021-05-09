fb4af7d0f9d484afce7d12823d4c66c44542de9e
atrusty <trusty.aaron.m@gmail.com>
2021-05-09 00:38:46 -0700

    init squad api call

--- index.js
+++ index.js
@@ -3,14 +3,60 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const axios = require('axios');
-
+const lib = require('lib')({token: null /* link an account to create an identity */});
 
 //Cors stuffs
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  });
+});
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`CT STATS app listening at http://localhost:${port}`)
-  });
+});
+
+//stats init testing
+app.get('/myStats', async (req, res) => {
+    
+    let result = await lib.halo.mcc['@0.0.11'].stats({
+        gamertag: "TR Trusty"
+    });
+
+    console.log(result);
+
+    retVal = {
+        hi: "hi :)"
+    };
+
+    res.send(retVal);
+});
+
+
+//stats squad
+app.get('/squadStats', async (req, res) => {
+
+    let result = await lib.halo.mcc['@0.0.11'].squad.activity({
+        gamertags: [
+            'TR Trusty',
+            'Cragle 3',
+            'A Pregnant Nunn',
+            'babaganoosh069',
+            'Girthy Jello',
+            'SirPuffingtin',
+            'ya boi nar',
+            'l swordzzz'
+        ],
+        minSquadSize: '2',
+        game: 'Halo 3'
+    });
+
+    console.log(result);
+
+
+    retVal = {
+        hi2: "hi2 :)"
+    };
+
+    res.send(retVal);
+
+});
