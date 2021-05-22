c343b918f74a889b6d743db9f818c7f8bd3e6d84
atrusty <trusty.aaron.m@gmail.com>
2021-05-21 17:23:25 -0700

    generate chart group component

--- front-end/ct-stats/src/app/app.module.ts
+++ front-end/ct-stats/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { ChartGroupComponent } from './chart-group/chart-group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    ChartGroupComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/ct-stats/src/app/chart-group/chart-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-group',
+  templateUrl: './chart-group.component.html',
+  styleUrls: ['./chart-group.component.css']
+})
+export class ChartGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
